42f48ddf22300e27cbbd75f9896512b4d4a36800
Gregor Becker <gregor.becker@fau.de>
2026-04-07 11:54:41 +0200

    Remove redundant defined macros

--- src/ext_simenv_capi.h
+++ src/ext_simenv_capi.h
@@ -9,23 +9,10 @@
 /* Used fixed data type widths */
 #include "ext_simenv_types.h"
 
-
-
-/* Provide macros as strings */
-#define _QUOTE1(name)				#name
-#define _QUOTE(name)				_QUOTE1(name)
-
-/* Macro concatination */
-#define EXPAND_CONCAT(name1,name2)	name1 ## name2
-#define CONCAT(name1,name2)			EXPAND_CONCAT(name1,name2)
-
-
-
 /* Define NULL if not defined */
 #ifndef NULL
  #define NULL    ((void *)0)
 #endif
-
 
 
 /* Static input, output information; vectorized signals are allowed */
