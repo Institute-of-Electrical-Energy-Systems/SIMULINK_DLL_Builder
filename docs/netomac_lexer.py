a6632c197908a3aed844792ad6bae07fed382f48
Becker <si89weva@fauad.fau.de>
2026-08-12 10:07:29 +0200

    Update netomac_lexer.py

--- docs/netomac_lexer.py
+++ docs/netomac_lexer.py
@@ -1,20 +1,29 @@
-from pygments.lexer import RegexLexer from pygments.token import Comment, Keyword, Name, Number, Text from pygments.lexers import _mappingclass NetomacLexer(RegexLexer):
-    name = "Netomac"aliases = ["netomac"]
+from pygments.lexer import RegexLexer
+from pygments.token import Comment, Keyword, Name, Number, Text
+
+
+class NetomacLexer(RegexLexer):
+    name = "Netomac"
+    aliases = ["netomac"]
+    filenames = ["*.net"]
 
     tokens = {
         "root": [
             (r"\$.*?$", Comment),
+            (r"!.*?$", Comment),
             (r"@@", Keyword),
             (r"#[A-Za-z_][A-Za-z0-9_]*", Name.Variable),
             (r"\b\d+(\.\d+)?([eE][+-]?\d+)?\b", Number),
-            (r"!.*?$", Comment),
             (r"\s+", Text),
             (r".", Text),
         ]
     }
 
+
 def setup(app):
-    from sphinx.highlighting import lexerslexers["netomac"] = NetomacLexer()
+    from sphinx.highlighting import lexers
+
+    lexers["netomac"] = NetomacLexer()
 
     return {
         "version": "1.0",
