8674f06272fd7159057822420c54a8875edf3222
Dominik Frauenknecht <dominik.frauenknecht@fau.de>
2026-09-07 16:27:24 +0200

    Update netomac_style.py
    Update netomac_style.py

--- docs/netomac_style.py
+++ docs/netomac_style.py
@@ -11,9 +11,9 @@ class NetomacStyle(Style):
     styles = {
         Text:            "#000000",
         Comment:         "noitalic #01893a",   # $ ... and ! ... lines
-        Keyword:         "bold #ff2640",     # [[Section]] headers
-        Operator:        "bold #a31515",     # @ and @@
-        Name.Variable:   "#267f99",          # #Vn, #Rc, #\MAC\...
-        String:          "#267f99",          # 'VSrc', 'IBR'
+        Keyword:         "#ff2640",     # [[Section]] headers
+        Operator:        "#dfdfe5",     # @ and @@
+        Name.Variable:   "#000000",          # #Vn, #Rc, #\MAC\...
+        String:          "#e96401",          # 'VSrc', 'IBR'
         Number:          "#000000",
     }
